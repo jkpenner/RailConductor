bf11b4ec58d463b142600d18d059d57ce5aae87d
Jacob Penner <jkpenner@live.com>
2026-01-27 15:19:27 -0800

    Move interactions into own scripts.

---
+++ scripts/interactions/Interactable.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace RailConductor;
+
+public abstract partial class Interactable : Area2D
+{
+    [Export]
+    public int InteractPriority { get; set; } = 0;
+
+    [Export]
+    public bool IsInteractEnabled { get; set; } = true;
+    
+    public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
+    {
+        if (@event is InputEventMouseButton { Pressed: true, ButtonIndex: MouseButton.Left })
+        {
+            Interactions.Instance?.NotifyInteraction(this);
+        }
+    }
+
+    public void Interact()
+    {
+        if (!IsInteractEnabled)
+        {
+            GD.PushWarning("Attempted to interact with disabled interactable.");
+            return;
+        }
+        
+        OnInteraction();
+    }
+
+    protected abstract void OnInteraction();
+}
---
+++ scripts/interactions/Interactions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+namespace RailConductor;
+
+public partial class Interactions : Node
+{
+    public static Interactions? Instance { get; private set; }
+    
+    private readonly HashSet<Interactable> _objects = [];
+
+    
+    public Action<Interactable>? OnInteract;
+
+    public override void _EnterTree()
+    {
+        if (Instance is not null && Instance != this)
+        {
+            QueueFree();
+            return;
+        }
+        
+        Instance = this;
+    }
+
+    public override void _ExitTree()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        if (_objects.Count == 0)
+        {
+            return;
+        }
+        
+        // Get the interactable with the highest priority.
+        var target = _objects
+            .OrderBy(i => i.InteractPriority)
+            .FirstOrDefault(i => i.IsInteractEnabled);
+        
+        _objects.Clear();
+        target?.Interact();
+    }
+
+    public void NotifyInteraction(Interactable interactable)
+    {
+        _objects.Add(interactable);
+    }
+}
