b46714d52adbe0f2ff3390c59d1ef18dd7bba580
Jacob Penner <jkpenner@live.com>
2026-02-04 19:16:52 -0800

    Update the handling of trains.

--- Main.cs
+++ Main.cs
@@ -14,28 +14,26 @@ public partial class Main : Node
         {
             return;
         }
+        
+        PlaceTrain(train, segment);
     }
 
-    public Train? SpawnTrain(int linkId, int nodeId)
+    public bool PlaceTrain(Train train, TrackSegment segment)
     {
-        if (track is null)
-        {
-            GD.PushError("Track is null");
-            return null;
-        }
-        
-        var graph = track.GetGraph();
-        
-        var link = graph.GetLink(linkId);
-        var node = graph.GetNode(nodeId);
+        var keyA = segment.EndA.GetTrackKey();
+        var keyB = segment.EndB.GetTrackKey();
 
-        if (link is null || node is null || !link.Contains(node))
+        var graph = track?.GetGraph();
+        var link = graph?.GetLink(keyA, keyB);
+        var node = graph?.GetNode(keyB);
+
+        if (graph is null || link is null || node is null || !link.Contains(node))
         {
             GD.PushError($"Failed to spawn train, invalid position details.");
-            return null;
+            return false;
         }
 
-        train?.SetTrack(graph, link, node);
-        return train;
+        train.SetTrack(graph, link, node);
+        return true;
     }
 }
--- scripts/train/Train.cs
+++ scripts/train/Train.cs
@@ -9,34 +9,46 @@ public partial class Train : Node2D
     private TrackLocation? _lead;
     private TrackLocation? _rear;
 
+    [Export]
+    public float TrainLength { get; set; } = 2f;
+
+    [Export]
+    public Node2D? LeadVisual { get; set; }
+
+    [Export]
+    public Node2D? RearVisual { get; set; }
+
 
     public void SetTrack(TrackGraph graph, TrackGraphLink link, TrackGraphNode face)
     {
         _graph = graph;
-        _lead = new TrackLocation(link, face);
-        _rear = new TrackLocation(link, face);
+        _lead = new TrackLocation(link, face); ;
+        _rear = graph.Move(_lead, -TrainLength);
     }
 
     public override void _Process(double delta)
     {
         var move = Input.GetAxis("move_backward", "move_forward");
 
-        if (_graph is not null)
+        if (_graph is not null && _lead is not null)
         {
-            if (_lead is not null)
-            {
-                _graph.Move(_lead, move);
-            }
-
-            if (_rear is not null)
-            {
-                _graph.Move(_rear, move);
-            }
+            _lead = _graph.Move(_lead, move);
+            _rear = _graph.Move(_lead, -TrainLength);
         }
-        
+
         // Sync Train movement based on its wheels.
         var wheel = _lead?.GetGlobalPosition() ?? GlobalPosition;
 
         GlobalPosition = wheel;
+
+        if (LeadVisual is not null && _lead is not null)
+        {
+            LeadVisual.GlobalPosition = _lead.GetGlobalPosition();
+        }
+
+        if (RearVisual is not null && _rear is not null)
+        {
+            RearVisual.GlobalPosition = _rear.GetGlobalPosition();
+        }
     }
 }
