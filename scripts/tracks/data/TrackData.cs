00eab9fffe41816857717993c2b54c8d48c3f93e
Jacob Penner <jkpenner@live.com>
2026-02-06 20:23:33 -0800

    Add utility methods to TrackData

--- scripts/tracks/data/TrackData.cs
+++ scripts/tracks/data/TrackData.cs
@@ -78,4 +78,116 @@ public partial class TrackData : Resource
 
         return minDist < 20f ? closest : -1;
     }
+
+    public (int, int) FindClosestLink(Vector2 position)
+    {
+        if (_nodes.Count < 2)
+        {
+            return (-1, -1);
+        }
+
+        var links = GetUniqueLinks();
+
+        if (links.Count == 0)
+        {
+            return (-1, -1);
+        }
+
+        var minDist = float.MaxValue;
+        var closestLink = (-1, -1);
+
+        foreach (var link in links)
+        {
+            var node1 = GetNode(link.Item1);
+            var node2 = GetNode(link.Item2);
+
+            if (node1 == null || node2 == null)
+            {
+                continue;
+            }
+
+            var dist = DistanceToSegment(position, node1.Position, node2.Position);
+
+            if (dist >= minDist)
+            {
+                continue;
+            }
+
+            minDist = dist;
+            closestLink = link;
+        }
+
+        return minDist < 20f ? closestLink : (-1, -1);
+    }
+
+    public float GetClosestLinkDistance(Vector2 position)
+    {
+        if (_nodes.Count < 2)
+        {
+            return float.MaxValue;
+        }
+
+        var links = GetUniqueLinks();
+
+        if (links.Count == 0)
+        {
+            return float.MaxValue;
+        }
+
+        var minDist = float.MaxValue;
+
+        foreach (var link in links)
+        {
+            var node1 = GetNode(link.Item1);
+            var node2 = GetNode(link.Item2);
+
+            if (node1 == null || node2 == null)
+            {
+                continue;
+            }
+
+            var dist = DistanceToSegment(position, node1.Position, node2.Position);
+
+            if (dist < minDist)
+            {
+                minDist = dist;
+            }
+        }
+
+        return minDist;
+    }
+
+    private HashSet<(int, int)> GetUniqueLinks()
+    {
+        var links = new HashSet<(int, int)>();
+
+        foreach (var node in _nodes.Values)
+        {
+            foreach (var linkId in node.Links)
+            {
+                var id1 = Mathf.Min(node.Id, linkId);
+                var id2 = Mathf.Max(node.Id, linkId);
+                links.Add((id1, id2));
+            }
+        }
+
+        return links;
+    }
+
+    private static float DistanceToSegment(Vector2 p, Vector2 a, Vector2 b)
+    {
+        var ab = b - a;
+        var ap = p - a;
+        
+        var len2 = ab.LengthSquared();
+        if (len2 == 0)
+        {
+            return p.DistanceTo(a);
+        }
+
+        var proj = ap.Dot(ab);
+        var t = Mathf.Clamp(proj / len2, 0f, 1f);
+        var projection = a + t * ab;
+        return p.DistanceTo(projection);
+    }
 }
