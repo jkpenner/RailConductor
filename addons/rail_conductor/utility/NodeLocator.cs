3bc7ae212eacc934d11d71d7b05e8d12d66c85f2
Jacob Penner <jkpenner@live.com>
2026-02-19 15:26:24 -0800

    Fix already connected to given callable error.

--- addons/rail_conductor/utility/NodeLocator.cs
+++ addons/rail_conductor/utility/NodeLocator.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using Godot;
 
-public class NodeLocator<T> where T : Node
+public partial class NodeLocator<T> : GodotObject where T : Node
 {
     public IReadOnlySet<T> Nodes => _nodes;
-    private readonly HashSet<T> _nodes = [];
+    private readonly HashSet<T> _nodes = new();
 
     private Node? _root;
+    private SceneTree? _sceneTree;
 
     public void SetRoot(Node? newRoot)
     {
@@ -18,10 +19,14 @@ public class NodeLocator<T> where T : Node
 
         if (_root is null) return;
 
-        _root.ChildEnteredTree += OnDirectChildEntered;
-        _root.ChildExitingTree += OnDirectChildExited;
+        _sceneTree = _root.GetTree();
 
-        // One-time full registration
+        if (_sceneTree is not null)
+        {
+            _sceneTree.NodeAdded += OnNodeAdded;
+            _sceneTree.NodeRemoved += OnNodeRemoved;
+        }
+
         RegisterSubtree(_root);
     }
 
@@ -29,94 +34,36 @@ public class NodeLocator<T> where T : Node
 
     private void DisconnectAll()
     {
-        if (_root is null)
+        if (_sceneTree is not null)
         {
-            return;
+            _sceneTree.NodeAdded -= OnNodeAdded;
+            _sceneTree.NodeRemoved -= OnNodeRemoved;
+            _sceneTree = null;
         }
 
-        _root.ChildEnteredTree -= OnDirectChildEntered;
-        _root.ChildExitingTree -= OnDirectChildExited;
-
-        // Clean up EVERY node we ever touched
-        UnregisterSubtree(_root);
+        _nodes.Clear();
     }
 
-    private void OnDirectChildEntered(Node node)
+    private void OnNodeAdded(Node node)
     {
         if (node is T t)
-        {
             _nodes.Add(t);
-        }
-
-        node.ChildEnteredTree += OnDeepChildEntered;
-        node.ChildExitingTree += OnDeepChildExited;
-
-        RegisterSubtree(node);
     }
 
-    private void OnDirectChildExited(Node node)
+    private void OnNodeRemoved(Node node)
     {
         if (node is T t)
-        {
             _nodes.Remove(t);
-        }
-
-        // Critical: clean up this whole branch
-        UnregisterSubtree(node);
-
-        node.ChildEnteredTree -= OnDeepChildEntered;
-        node.ChildExitingTree -= OnDeepChildExited;
     }
 
-    private void OnDeepChildEntered(Node node)
-    {
-        if (node is T t)
-        {
-            _nodes.Add(t);
-        }
-    }
-
-    private void OnDeepChildExited(Node node)
-    {
-        if (node is T t)
-        {
-            _nodes.Remove(t);
-        }
-    }
-
-    // Register all T nodes + add deep listeners to EVERY node in subtree
     private void RegisterSubtree(Node node)
     {
+        if (!GodotObject.IsInstanceValid(node)) return;
+
         if (node is T t)
-        {
             _nodes.Add(t);
-        }
-
-        // Subscribe to this node's children changes (so we catch future deep adds)
-        node.ChildEnteredTree += OnDeepChildEntered;
-        node.ChildExitingTree += OnDeepChildExited;
 
         foreach (var child in node.GetChildren())
-        {
             RegisterSubtree(child);
-        }
-    }
-
-    // Remove all T nodes + unsubscribe from EVERY node in this subtree
-    private void UnregisterSubtree(Node node)
-    {
-        if (node is T t)
-        {
-            _nodes.Remove(t);
-        }
-
-        // Unsubscribe from this node's child signals
-        node.ChildEnteredTree -= OnDeepChildEntered;
-        node.ChildExitingTree -= OnDeepChildExited;
-
-        foreach (var child in node.GetChildren())
-        {
-            UnregisterSubtree(child);
-        }
     }
 }
